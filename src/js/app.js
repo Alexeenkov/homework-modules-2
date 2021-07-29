f569384f056e3b4d4051321197e6de49e795b2fe
Ilya Alekseenkov <rmfighht@yandex.ru>
2021-07-29 19:38:43 +0300

    Небольшие доработки

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import Game, {GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from './game';
 import Character from './domain';
 
+console.log('app worked');
+
 const game = new Game();
 game.start();
