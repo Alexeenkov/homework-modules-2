4f4163793ef984da50146f21e8676d21799f7505
Ilya Alekseenkov <rmfighht@yandex.ru>
2021-07-31 15:06:41 +0300

    Адаптировал под первую задачу

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,1 @@
-import Game, {GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from './game';
-import Character from './domain';
-
 console.log('app worked');
-
-const game = new Game();
-game.start();
